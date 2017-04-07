d734493fde5094525d3fcffb0496388012fd7642
xuezheng-zhuzibaijia <2770837735@qq.com>
2017-04-07 15:50:00 +0800

    organize the orginal architecture of the project

---
+++ Btree/btree.h
@@ -0,0 +1,7 @@
+#ifndef BTREE_H_INCLUDED
+#define BTREE_H_INCLUDED
+#include <stdio.h>
+#include <string.h>
+#include <libmemobj.h>  //compile command would be 'cc -std=gnu99　... -lpmemobj -lpmem'
+
+#endif // BTREE_H_INCLUDED
---
+++ Btree/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "btree.h"
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
